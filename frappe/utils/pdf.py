fa752197f85732125ada7a7a57006426b52e8479
Ubuntu <labotecherp@ERPNext-VM.rksqbydrrpdeld5h0uhr1prc1f.bx.internal.cloudapp.net>
2020-03-21 06:16:52 +0000

    orienttion

--- frappe/utils/pdf.py
+++ frappe/utils/pdf.py
@@ -129,7 +129,7 @@ def read_options_from_html(html):
 	# use regex instead of soup-parser
 	for attr in ("margin-top", "margin-bottom", "margin-left", "margin-right", "page-size", "header-spacing", "orientation"):
 		try:
-			pattern = re.compile(r"(\.print-format)([\S|\s][^}]*?)(" + str(attr) + r":)(.+)(mm;)")
+			pattern = re.compile(r"(\.print-format)([\S|\s][^}]*?)(" + str(attr) + r":)(.+)(mm)?;")
 			match = pattern.findall(html)
 			if match:
 				options[attr] = str(match[-1][3]).strip()
